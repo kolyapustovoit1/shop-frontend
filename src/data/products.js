501d22fb80dc9223f89591e32329eb3823c32956
kolya <kolyapustovoit1@gmail.com>
2025-07-10 22:33:45 +0300

    initial commit

---
+++ src/data/products.js
@@ -0,0 +1,26 @@
+export const products = [
+    {
+      id: 1,
+      name: 'Ноутбук "Космос"',
+      price: 35000,
+      image: 'https://via.placeholder.com/250?text=Laptop'
+    },
+    {
+      id: 2,
+      name: 'Смартфон "Галактика"',
+      price: 25000,
+      image: 'https://via.placeholder.com/250?text=Smartphone'
+    },
+    {
+      id: 3,
+      name: 'Навушники "Звукова Хвиля"',
+      price: 4500,
+      image: 'https://via.placeholder.com/250?text=Headphones'
+    },
+    {
+      id: 4,
+      name: 'Смарт-годинник "Час"',
+      price: 8200,
+      image: 'https://via.placeholder.com/250?text=Smartwatch'
+    }
+  ];
